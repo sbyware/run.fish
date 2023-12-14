be8dbab08fc1c775be54f3528f46ca969c0825ab
sby051 <91495085+sby051@users.noreply.github.com>
2023-12-14 13:57:02 +0000

    update

--- conf.d/run.fish
+++ conf.d/run.fish
@@ -1,5 +1,5 @@
 function _run_install --on-event run_install
-    echo "[run installer] setting up run variables"
+    echo "[run.fish installer] setting up run variables"
     set -gx run__allowed_extensions "fish" "js" "bun.ts" "py" "sh"
     set -gx run__allowed_executables "fish" "node" "bun" "python3" "bash"
     set -gx run__dir "$HOME/.run"
@@ -8,21 +8,21 @@ function _run_install --on-event run_install
     set -gx run__log_file "$run__dir/.log"
     set -gx run__version "1.0.0"
     set -gx run__repo_url "https://github.com/sby051/run.fish"
-    echo "[run installer] installing run v$run__version"
-    echo "[run installer] creating run directory"
+    echo "[run.fish installer] installing run v$run__version"
+    echo "[run.fish installer] creating run directory"
     mkdir -p $run__script_dir
     touch $run__history_file
     touch $run__log_file
-    echo "[run installer] copying scripts to $run__dir"
+    echo "[run.fish installer] copying scripts to $run__dir"
     cp -r (pwd)"/scripts" $run__dir
-    echo "[run installer] installed run v$run__version"
+    echo "[run.fish installer] installed run v$run__version"
 end
 
 function _run_uninstall --on-event run_uninstall
-    echo "[run uninstaller] uninstalling run v$run__version"
-    echo "[run uninstaller] removing run directory"
+    echo "[run.fish uninstaller] uninstalling run v$run__version"
+    echo "[run.fish uninstaller] removing run directory"
     rm -rf $run__dir
-    echo "[run uninstaller] removing run variables"
+    echo "[run.fish uninstaller] removing run variables"
     set -e run__allowed_extensions
     set -e run__allowed_executables
     set -e run__dir
@@ -30,6 +30,6 @@ function _run_uninstall --on-event run_uninstall
     set -e run__script_dir
     set -e run__log_file
     set -e run__repo_urls
-    echo "[run uninstaller] uninstalled run v$run__version"
+    echo "[run.fish uninstaller] uninstalled run v$run__version"
     set -e run__version
 end
