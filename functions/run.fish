cd8d348bd5d6a3a223eb44694d82a8c7cf4f9d34
sby051 <91495085+sby051@users.noreply.github.com>
2023-12-30 01:07:54 +0000

    update

--- functions/run.fish
+++ functions/run.fish
@@ -51,7 +51,7 @@ function run.rm --description "Removes a script in the $run__script_dir director
     set --local script_name $argv[1]
 
     if test (count $argv) -ne 1
-        echo "[run-delete] Usage: run-delete <script_name>"
+        echo "[run.rm] Usage: run.rm <script_name>"
         return 1
     end
 
@@ -59,13 +59,13 @@ function run.rm --description "Removes a script in the $run__script_dir director
 
     for extension in $run__allowed_extensions
         if test -f "$script_path.$extension"
-            echo "[run-delete] Deleting $script_path.$extension..."
+            echo "[run.rm] Deleting $script_path.$extension..."
             rm "$script_path.$extension"
             return 0
         end
     end
 
-    echo "[run-delete] No script found with name $script_name"
+    echo "[run.rm] No script found with name $script_name"
     return 1
 end
 
@@ -81,17 +81,17 @@ function run.ln --description "Create symbolic link to an existing script in the
     end
 
     if test (count $argv) -lt 1
-        echo "[run-link] Usage: run-link <script_path> <script_alias (optional)>"
+        echo "[run.ln] Usage: run.ln <script_path> <script_alias (optional)>"
         return 1
     end
 
     if test ! -f $script_path
-        echo "[run-link] Error: $script_path does not exist"
+        echo "[run.ln] Error: $script_path does not exist"
         return 1
     end
 
     if test ! -x $script_path
-        echo "[run-link] Error: $script_path is not executable"
+        echo "[run.ln] Error: $script_path is not executable"
         return 1
     end
 
@@ -103,20 +103,20 @@ function run.ln --description "Create symbolic link to an existing script in the
     end
 
     if test $valid_extension -ne 0
-        echo "[run-link] Error: $script_extension is not a valid extension"
+        echo "[run.ln] Error: $script_extension is not a valid extension"
         return 1
     end
 
     if test -f "$run__script_dir/$script_name.$script_extension"
-        echo "[run-link] Error: $run__script_dir/$script_name.$script_extension already exists"
+        echo "[run.ln] Error: $run__script_dir/$script_name.$script_extension already exists"
         return 1
     end
 
-    echo "[run-link] Creating symbolic link '$run__script_dir/$script_name.$script_extension'..."
+    echo "[run.ln] Creating symbolic link '$run__script_dir/$script_name.$script_extension'..."
 
     ln -s $script_path "$run__script_dir/$script_name.$script_extension"
 
-    echo "[run-link] Done! Created '$run__script_dir/$script_name.$script_extension'."
+    echo "[run.ln] Done! Created '$run__script_dir/$script_name.$script_extension'."
 end
 
 function run.history --description "Lists the history of scripts run with run"
@@ -128,7 +128,7 @@ function run.log --description "Displays the run log file"
 end
 
 function run.ls --description "Lists all scripts in the $run__script_dir directory"
-    echo "[run-list] Scripts in $run__script_dir:"
+    echo "[run.ls] Scripts in $run__script_dir:"
     for extension in $run__allowed_extensions
         for script in $run__script_dir/*.$extension
             echo "- $(basename $script .$extension)"
@@ -138,7 +138,7 @@ end
 
 function run.edit --description "Opens the script in the $run__script_dir directory in $EDITOR"
     if test (count $argv) -ne 1
-        echo "[run-edit] Usage: run-edit <script_name>"
+        echo "[run.edit] Usage: run.edit <script_name>"
         return 1
     end
 
@@ -147,13 +147,13 @@ function run.edit --description "Opens the script in the $run__script_dir direct
 
     for extension in $run__allowed_extensions
         if test -f "$script_path.$extension"
-            echo "[run-edit] Opening $script_path.$extension in $EDITOR..."
+            echo "[run.edit] Opening $script_path.$extension in $EDITOR..."
             $EDITOR "$script_path.$extension"
             return 0
         end
     end
 
-    echo "[run-edit] No script found with name $script_name"
+    echo "[run.edit] No script found with name $script_name"
     return 1
 end
 
@@ -162,14 +162,14 @@ function run.new --description "Creates a new script in the $run__script_dir dir
     set --local script_type $argv[2]
 
     if test (count $argv) -ne 2
-        echo "[run-create] Usage: run-create <script_name> <script_type>"
+        echo "[run.new] Usage: run.new <script_name> <script_type>"
         return 1
     end
 
     set --local executor_path (which $script_type)
 
     if test -z "$executor_path"
-        echo "[run-create] Couldn't find binary for $script_type"
+        echo "[run.new] Couldn't find binary for $script_type"
         return 1
     end
 
@@ -180,21 +180,21 @@ function run.new --description "Creates a new script in the $run__script_dir dir
     end
 
     if test $executor_path != $executable
-        echo "[run-create] Error: $script_type is not an allowed executable"
+        echo "[run.new] Error: $script_type is not an allowed executable"
         return 1
     end
 
     if test -f "$run__script_dir/$script_name.$script_extension"
-        echo "[run-create] $run__script_dir/$script_name.$script_extension already exists"
+        echo "[run.new] $run__script_dir/$script_name.$script_extension already exists"
         return 1
     end
 
-    echo "[run-create] Creating '$run__script_dir/$script_name.$script_extension'..."
+    echo "[run.new] Creating '$run__script_dir/$script_name.$script_extension'..."
 
     echo "#!$executor_path" > "$run__script_dir/$script_name.$script_extension"
     chmod +x "$run__script_dir/$script_name.$script_extension"
 
-    echo "[run-create] Done! Created '$run__script_dir/$script_name.$script_extension'. Opening in $EDITOR..."
+    echo "[run.new] Done! Created '$run__script_dir/$script_name.$script_extension'. Opening in $EDITOR..."
 
     $EDITOR "$run__script_dir/$script_name.$script_extension"
 end
@@ -210,13 +210,13 @@ function __print_help --description "Prints the help message"
     echo "    --help: Prints this help message"
     echo "    --version: Prints the version of run"
     echo "Additional commands:"
-    echo "  run.delete <script_name>: Deletes a script in the $run__script_dir directory"
-    echo "  run.link <script_path> <script_alias (optional)>: Create symbolic link to an existing script in the $run__script_dir directory"
+    echo "  run.rm <script_name>: Deletes a script in the $run__script_dir directory"
+    echo "  run.ln <script_path> <script_alias (optional)>: Create symbolic link to an existing script in the $run__script_dir directory"
+    echo "  run.new <script_name> <script_type>: Creates a new script in the $run__script_dir directory"
+    echo "  run.ls: Lists all scripts in the $run__script_dir directory"
     echo "  run.history: Lists the history of scripts run with run"
     echo "  run.log: Displays the run log file"
-    echo "  run.list: Lists all scripts in the $run__script_dir directory"
     echo "  run.edit <script_name>: Opens the script in the $run__script_dir directory in $EDITOR"
-    echo "  run.create <script_name> <script_type>: Creates a new script in the $run__script_dir directory"
     echo ""
-    echo "Examples: run.create test node, run.link /path/to/script.sh, run.edit test"
+    echo "Examples: run.new test node, run.ln /path/to/script.sh, run.edit test"
 end
