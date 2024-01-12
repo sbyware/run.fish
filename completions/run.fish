99649856c0ce3879a2f862fb6ccf747482c2a0bc
sby051 <91495085+sby051@users.noreply.github.com>
2024-01-12 18:19:42 +0000

    uppdate

--- completions/run.fish
+++ completions/run.fish
@@ -1,8 +1,8 @@
 function __fetch_run_scripts_without_extension
-    for file in $run__script_dir/*.*
-        set -l script_name (string replace -r "\.($run__allowed_extensions)\$" "" -- (basename $file))
-        echo $script_name
+    for file in $run__script_dir/*
+        set --local without_extension (string match -r "(.*)\." -- $file)[1]
+        echo $without_extension
     end
 end
 
-complete -c run -a '(__fetch_run_scripts_without_extension)' --description 'Available scripts in $run__script_dir'
+complete -c run -a '(__fetch_run_scripts_without_extension)' --description 'Available scripts'
