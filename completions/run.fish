671c63fa0121b3323f64962af79dbf56765e7a42
sby051 <91495085+sby051@users.noreply.github.com>
2024-01-12 18:17:30 +0000

    update

--- completions/run.fish
+++ completions/run.fish
@@ -1,8 +1,8 @@
-function __fetch_run_scripts
-    set -l scripts (string replace -r "\.($run__allowed_extensions)\$" "" -- (basename -a $run__script_dir/*))
-    for script in $scripts
-        echo $script
+function __fetch_run_scripts_without_extension
+    for file in $run__script_dir/*.*
+        set -l script_name (string replace -r "\.($run__allowed_extensions)\$" "" -- (basename $file))
+        echo $script_name
     end
 end
 
-complete -c run -a '(__fetch_run_scripts)' --description 'Available scripts'
+complete -c run -a '(__fetch_run_scripts_without_extension)' --description 'Available scripts in $run__script_dir'
