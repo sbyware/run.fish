390d7d707935ace92bfeb5b38e128a139486061c
sby051 <91495085+sby051@users.noreply.github.com>
2024-01-12 18:15:09 +0000

    update

--- completions/run.fish
+++ completions/run.fish
@@ -1,26 +1,8 @@
-# fish/completions/run.fish
-function __fish_run_script_names
-    cat $run__history_file
-end
-
-function __fish_run_complete
-    __fish_use_subcommand
-    set cmd (commandline -opc)
-    set current (commandline -ct)
-
-    switch $cmd
-        case "run"
-            if string match -q -- "--*" $current
-                return
-            end
-
-            set -l script_names (__fish_run_script_names)
-            for script in $script_names
-                echo $script
-            end
-        case "*"
-            return
+function __fetch_run_scripts
+    set -l scripts (string replace -r "\.($run__allowed_extensions)\$" "" -- (basename -a $run__script_dir/*))
+    for script in $scripts
+        echo $script
     end
 end
 
-complete -c run -f -a "(__fish_run_complete)"
+complete -c run -a '(__fetch_run_scripts)' --description 'Available scripts'
